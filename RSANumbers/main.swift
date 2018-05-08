95cae141b854165b1c7927f319f61f8b6d71cc81
Da, Annie <ada@lcs-mba3107.lcs.on.ca>
2018-05-08 08:44:10 -0400

    complete

--- RSANumbers/main.swift
+++ RSANumbers/main.swift
@@ -11,8 +11,85 @@ import Foundation
 // INPUT
 // Collect and filter user input here
 
-// PROCESS
-// Implement the primary logic of the problem here
+var lowerLimitRange = 0
+var upperLimitRange = 0
+while 1 == 1 {
+    
+    // Prompt the user
+    print("enter the lower limit:")
+    
+     // Test #1: Make sure input is not nil
+    guard let givenInput = readLine() else {
+        
+        continue    // continue with next iteration of the loop
+        }
 
-// OUTPUT
-// Report results to the user here
+     // Test #2: Make sure input can be represented as an integer
+    guard let givenInteger = Int(givenInput) else {
+        
+        continue    // continue with next iteration of the loop
+        }
+
+    // Test #3: Make sure input is in correct range
+    if givenInteger < 1 || givenInteger > 1000 {
+        
+        continue
+        }
+        // Input is valid, so assign it to the variable to be used outside this loop
+        
+     lowerLimitRange = givenInteger
+        
+        break
+    
+}
+
+while 1 == 1 {
+    
+     // Prompt the user
+     print("enter the upper limit:")
+
+    // Test #1: Make sure input is not nil
+     guard let givenInput = readLine() else {
+    
+     continue    // continue with next iteration of the loop
+     }
+
+    // Test #2: Make sure input can be represented as an integer
+     guard let givenInteger = Int(givenInput) else {
+    
+     continue    // continue with next iteration of the loop
+     }
+
+// Test #3: Make sure input is in correct range
+if givenInteger < 1 || givenInteger > 1000 {
+    
+    continue
+     }
+// Input is valid, so assign it to the variable to be used outside this loop
+
+    upperLimitRange = givenInteger
+
+break
+
+  }
+
+// PROCESS
+  var RSA = 0
+    for numbers in lowerLimitRange ... upperLimitRange {
+    var diviser = 0
+       for i in 1 ... numbers{
+       if numbers % i == 0 {
+       diviser += 1
+        
+        }
+        }
+    if diviser == 4 {
+    RSA += 1
+
+        }
+}
+    
+    // OUTPUT
+    // Report results to the user here
+    print("The number of RSA is \(RSA)")
+
